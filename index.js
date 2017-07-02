d640d506157662d1000d010a6b6a82d356e2889e
Elad Shaham <eshaham@paypal.com>
2017-07-02 13:01:19 +0300

    post to server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const request = require('request');
 const scanner = require('./scanner');
+
 scanNow();
 
 async function scanNow() {
   const list = await scanner.scan();
-  console.log(list);
+  const req = {
+    entries: list
+  }
+  request.post({ url: 'https://meepo-api.herokuapp.com/entries', body: req, json: true });
 }
