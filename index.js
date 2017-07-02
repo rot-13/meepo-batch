59d6fafea2cdfcec19cbd4ac3187b929890fa186
Elad Shaham <eshaham@paypal.com>
2017-07-02 13:27:38 +0300

    add log

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ async function scanNow() {
   const req = {
     entries: list
   }
+  console.log(`sending ${list.length} entries to server`);
   request.post({ url: 'https://meepo-api.herokuapp.com/entries', body: req, json: true });
 }
